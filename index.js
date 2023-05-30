18c857bca9699ff76ffec69b47769dda9c7a9927
bluesky7221 <bluesky7221@naver.com>
2023-05-30 21:57:29 +0900

    another3

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const express = require('express');
 // Express 앱을 초기화합니다.
 const app = express();
 const port = 3000;
-const c = 'cc';
+const c = 'd';
+const d = 'e';
 
 // 루트 URL('/')에 대한 GET 요청을 처리합니다.
 app.get('/', (req, res) => {
@@ -13,6 +14,10 @@ app.get('/', (req, res) => {
 
 // 웹 서버를 시작하고 주어진 포트에서 실행합니다.
 app.listen(port, () => {
-  console.log('이것은 main의 세번째 커밋입니다.');
+  console.log('내가 맘대로 짜놓은 것');
+  console.log(d);
   console.log(`Express 앱이 http://localhost:${port}에서 실행 중입니다.`);
+  for (var i = 0; i < 5; i++ ) {
+    console.log(d);
+  }
 });
